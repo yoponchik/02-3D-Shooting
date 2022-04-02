f2ea45ce03caf105e7aa2fd8bee607d30cf1f0b4
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-04-02 23:31:49 +0900

    Update PlayerMove.cs
    -Change to CharacterController Movement
    -Jump

--- Assets/02. Scripts/PlayerMove.cs
+++ Assets/02. Scripts/PlayerMove.cs
@@ -4,20 +4,67 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    //Move
     public float speed = 5;
+
+    //Jump
+    public float yVelocity = 0;
+    public int maxJumpCount = 2;
+    int jumpCount = 0;
+    [SerializeField] float jumpPower = 10;
+    [SerializeField] float gravity = -9.81f;
+
+    //Character Controller
+    CharacterController cc;
+
+    private void Start() {
+        cc = GetComponent<CharacterController>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Jump();
+    }
+
+    void Move() {
         //Get User input from the movement keys
         float hori = Input.GetAxis("Horizontal");
         float verti = Input.GetAxis("Vertical");
 
-        //make inputs into one vector to move the Player
-        //Vector3 direction = Vector3.right * hori + Vector3.forward * verti;
-        Vector3 direction = new Vector3(hori, 0, verti);
-        direction.Normalize();                                                          //Normalize
+        #region Transform Movement
+        /*        
+                //make inputs into one vector to move the Player
+                //Vector3 direction = Vector3.right * hori + Vector3.forward * verti;
+                Vector3 direction = new Vector3(hori, 0, verti);
+                direction.Normalize();                                                          //Normalize
 
-        transform.position += direction * speed * Time.deltaTime;                       //Move the Player
+                transform.position += direction * speed * Time.deltaTime;                       //Move the Player*/
+        #endregion Transform Movement
+
+        //velocity in the air
+        if (cc.isGrounded) {        //When on the ground
+            jumpCount = 0;
+            yVelocity = 0;
+        }
+        else {                      //When in the air
+            yVelocity += gravity * Time.deltaTime;
+        }
+
+        //Character Controller Movement
+        Vector3 direction = new Vector3 (hori, 0, verti);
+        direction = Camera.main.transform.TransformDirection(direction);            //Move in the direction the camera is pointing in
+        direction.Normalize();
+        Vector3 velocity = direction * speed;
+        velocity.y = yVelocity;                                         //set y component of velocity
+        cc.Move(velocity * Time.deltaTime);                             //move
+    }
+
+    void Jump() {
+        if (jumpCount < maxJumpCount && Input.GetButtonDown("Jump")) {
+            yVelocity = jumpPower;
+            jumpCount++;
+        }
     }
 }
