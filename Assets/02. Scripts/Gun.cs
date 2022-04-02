42213affd913ac9baac380ab9390ee020a9a1138
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-04-02 22:17:46 +0900

    Gun Script

---
+++ Assets/02. Scripts/Gun.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    Camera cam;
+    public GameObject bulletImpact;
+    ParticleSystem ps;
+
+    void Start()
+    {
+        cam = Camera.main;
+        ps = bulletImpact.GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) { 
+            Ray ray = new Ray(cam.transform.position, cam.transform.forward);               //Create a ray from the camera pointing out
+            RaycastHit hitInfo;                                                             //Create a Raycast variable
+            if (Physics.Raycast(ray, out hitInfo)) { 
+                bulletImpact.transform.position = hitInfo.point;                            //place the particle VFX at the hitpoint
+                bulletImpact.transform.forward = hitInfo.normal;
+                ps.Stop();
+                ps.Play();                                                                  //because it is a particle system effect, we need to give it a play command
+            }
+        }
+    
+    }
+
+}
